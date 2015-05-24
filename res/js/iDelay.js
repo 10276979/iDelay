70b0201c9351e564b591669f93a8a86ec33a47e9
10276979 <hideweb@sina.com>
2015-05-24 21:50:59 +0800

    iDelay

--- res/js/iDelay.js
+++ res/js/iDelay.js
@@ -1,1 +1,17 @@
-﻿
+﻿/*
+ * iDelay - 懒加载插件
+ *
+ * Create person: 张飞超
+ *
+ * Modify Time: 2015-05-24 21:48
+ *
+ * Version: 1.0.0
+ *
+ */
+
+; (function (win, doc) {
+    "use strict";
+
+
+
+})(window, document);
